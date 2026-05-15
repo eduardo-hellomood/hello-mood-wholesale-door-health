83b2992df845e9ee2b12b2ded939b8b1004c4c51
Eduardo <eduardo@hellomood.co>
2026-05-14 20:04:23 -0400

    Move Weekly toggle next to chart title

--- wholesale_door_health.py
+++ wholesale_door_health.py
@@ -133,7 +133,7 @@ st.markdown("<div style='margin-top:24px'></div>", unsafe_allow_html=True)
 chart_l, chart_r = st.columns([6, 4])
 
 with chart_l:
-    title_col, toggle_col = st.columns([5, 1])
+    title_col, toggle_col, _ = st.columns([4, 1, 1])
     with toggle_col:
         use_weekly = st.toggle("Weekly", value=False)
     if use_weekly:
